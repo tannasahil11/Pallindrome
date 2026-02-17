466913bc385265958635e69d0de4ae3489c6aa7a
tannasahil11 <sahiltanna@Sahils-MacBook-Air.local>
2026-02-16 20:18:44 -0400

    Code style clean up

--- palindrome.py
+++ palindrome.py
@@ -3,12 +3,13 @@ Validates strings as palindromes.
 """
 from collections import deque
 def is_plalindrome(word):
-    if type(word) != str:
+    """Checks if the entered word reads the same backwards."""
+    if not isinstance(word, str):
         raise ValueError("Input not a string")
     if len(word) == 0:
         return False
     dq = deque(word)
-    while( len(dq) > 1):
-        if( dq.pop().casefold() != dq.popleft().casefold()):
+    while len(dq) > 1:
+        if dq.pop().casefold() != dq.popleft().casefold():
             return False
     return True
--- test_palindrome.py
+++ test_palindrome.py
@@ -6,37 +6,46 @@ from palindrome import is_plalindrome
 
 #Step 1
 def test_is_plaindrome_with_int():
+    """Raises ValueError if the user enters an int"""
     with pytest.raises(ValueError):
         is_plalindrome(1881)
 
 def test_is_plaindrome_with_float():
+    """Raises ValueError if the user enters an float"""
     with pytest.raises(ValueError):
         is_plalindrome(13.31)
 
 #Step 2
 def test_is_palindrome_with_empty_string():
-    assert is_plalindrome("") == False
+    """Returns False when an empty string is entered"""
+    assert is_plalindrome("") is False
 
 #Step 3
 def test_is_palindrome_with_one_character():
-    assert is_plalindrome("a") == True
+    """Returns True for all the single string characters"""
+    assert is_plalindrome("a") is True
 
 #Step 4
 def test_is_palindrome_with_bb():
-    assert is_plalindrome("bb") == True
+    """Returns True for all the palindromes with even length"""
+    assert is_plalindrome("bb") is True
 
 #Step 5
 def test_is_palindrome_with_abc():
-    assert is_plalindrome("abc") == False
+    """Returns False for non-palindrome words"""
+    assert is_plalindrome("abc") is False
 
 #Step 6
 def test_is_palindrome_with_laval():
-    assert is_plalindrome("laval") == True
+    """Returns True for all the palindromes with odd length"""
+    assert is_plalindrome("laval") is True
 
 #Step 7
 def test_is_palindrome_with_toronto():
-    assert is_plalindrome("toronto") == False
+    """Returns False for non-palindrome words"""
+    assert is_plalindrome("toronto") is False
 
-#Step 8 
+#Step 8
 def test_is_plalindrome_with_sentence():
-    assert is_plalindrome("Able was I ere I saw Elba") == True
+    """Returns True for all the palindromes with different letter case"""
+    assert is_plalindrome("Able was I ere I saw Elba") is True
